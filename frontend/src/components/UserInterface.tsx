cde148129321ef38651bc065d59a6fad0a3586b6
luiz.carrion <luiz.carrion@ip.tv>
2024-02-24 11:57:30 -0300

    Get backend name and display

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -45,8 +45,38 @@ const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
     }, [backendName, apiUrl])
 
     return (
-        <div className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
-            test
+        <div 
+            className= {`
+                user-interface 
+                ${bgColor} 
+                ${backendName} 
+                w-full 
+                max-w-md 
+                p-4 
+                my-4 
+                rounded shadow
+            `}
+        >
+            <img 
+                src={`/${backendName}_logo.svg`} 
+                alt={`${backendName} Logo`} 
+                className="w-20 h-20 mb-6 mx-auto"
+            />
+            
+            <h2 
+                className="
+                    text-x1 
+                    font-bold 
+                    text-center 
+                    text-white 
+                    mb-6
+                "
+            >
+                    {`${backendName.charAt(0).toUpperCase() + backendName.slice(1)} Backend`}
+            </h2>
+
+            { /* Display Users */ }
+            
         </div>
     );
 }
